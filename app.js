710b4265ca390d72cb608d9c33a4a5c5ce85788b
Rodolfo Neves <roddas360@gmail.com>
2021-05-29 14:23:42 +0100

    Minor updates

--- app.js
+++ app.js
@@ -9,5 +9,8 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use('/',require('./routes/me'));
+app.use((request,response) =>{
+    response.status(404).render("404");
+});
 
 app.listen(PORT,console.log(`The server is running at ${PORT}`));
