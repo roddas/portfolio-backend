dc0ee23416b8a164c98de9530c242a06a77a500a
Roddas <roddas360@gmail.com>
2021-06-24 18:53:39 +0100

    Imroving the database async functions

---
+++ DatabaseClass.js
@@ -0,0 +1,118 @@
+const {dbConnect} = require('./data');
+
+const FAILURE = 'FAILURE';
+const SUCCESS = 'SUCCESS';
+
+class Database
+{
+    constructor()
+    {
+        this.result = {};
+    }
+    setResult(type,message)
+    {
+        this.result.type = type;
+        this.result.message = message;
+    }
+    getResult()
+    {
+        return this.result;
+    }
+    async getAllElements(table)
+    {
+       const QUERY = `SELECT * FROM ${table};`;
+       let connection = await dbConnect();
+       const [rows] = await connection.query(QUERY);
+       await connection.release();
+       return rows;
+    }
+    async getElementById(table,id_field,id)
+    {
+        const QUERY = `SELECT * FROM ${table} WHERE ${id_field} = ?;`;
+        let connection = await dbConnect();
+        const [rows] = await connection.query(QUERY,[id]);
+        await connection.release();
+        return rows[0];
+    }
+    getIdKey(object)
+    {
+        let i = 1;
+        let idName = undefined;
+        for (let a in object)
+        {
+            if (i++ == 1)
+            {
+                idName = a;
+                break;
+            }
+        }
+        return idName;
+    }
+    async removeElementById(table,object,idValue)
+    {
+        const idField = this.getIdKey();
+        const QUERY = `DELETE * FROM ${table} WHERE ${idField} = ?;`;
+        let connection = await dbConnect();
+        const [rows] = await connection.query(QUERY, [idValue]);
+        await connection.release();
+    }
+    async insertElement(table,elements)
+    {
+        let query = `INSERT INTO ${table} VALUES (NULL,`;
+        const SIZE = elements.length;
+        for(let a = 0; a < SIZE-1;a++)
+        {
+            query+='?,';
+        }
+        query += '?);';
+        console.log(query);
+        let connection = await dbConnect();
+        try
+        {
+            if(SIZE > 1)
+            {
+                await connection.query(query, elements);
+            }else
+            {
+                await connection.query(query, [elements]);
+            }   
+        }
+        catch(e)
+        {
+            console.error(e);
+        }finally
+        {
+            await connection.release();
+        }
+    }
+    async insertElement(table,elements)
+    {
+        let query = `INSERT INTO ${table} VALUES (NULL,`;
+        const SIZE = elements.length;
+        for(let a = 0; a < SIZE-1;a++)
+        {
+            query+='?,';
+        }
+        query += '?);';
+        console.log(query);
+        let connection = await dbConnect();
+        try
+        {
+            if(SIZE > 1)
+            {
+                await connection.query(query, elements);
+            }else
+            {
+                await connection.query(query, [elements]);
+            }   
+        }
+        catch(e)
+        {
+            console.error(e);
+        }finally
+        {
+            await connection.release();
+        }
+    }
+}
+module.exports = {Database};
