605d79cdab5af0f0512d0d937e47866d07919200
Roddas <roddas360@gmail.com>
2021-06-24 18:53:55 +0100

    File for metadata content store

---
+++ data.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv').config();
+const mysql = require('mysql2/promise');
+
+async function dbConnect()
+{
+    const pool =  mysql.createPool({
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME
+    });
+    return await pool.getConnection();
+}
+
+module.exports = {dbConnect};
