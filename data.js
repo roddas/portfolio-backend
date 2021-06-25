a6906a1238b1a987aa59573bb242b5ecabff9e03
Roddas <roddas360@gmail.com>
2021-06-25 18:31:37 +0100

    Little updates

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const dotenv = require('dotenv').config();
+const dotenv = require('dotenv').config({ path: __dirname+'/.env' });
 const mysql = require('mysql2/promise');
 
 async function dbConnect()
