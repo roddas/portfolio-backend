bd95dfffcb97f9869d25cf49eb7ea63e69449b54
Roddas <roddas360@gmail.com>
2021-06-29 23:01:52 +0100

    Updating the admin page

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,11 +1,72 @@
 const express = require('express');
+const {Database} = require('../../DatabaseClass');
+const NodeCache = require('node-cache');
 
 var router = express.Router();
 
-router.get('/',(request,response) =>{
+router.get('/',async (request,response) =>{
+    const nodeCache = new NodeCache();
     if (request.session.token === (process.env.TOKEN + process.env.TOKEN2) )
     {
-        response.render('admin/index',{title : 'Admin Page'});
+        var formacaoAcademica = undefined;
+        var conhecimentosTecnicos = undefined;
+        var contactos = undefined;
+        var experiencia = undefined;
+        var idiomas = undefined;
+        var linguagensFerramentas = undefined;
+        var projectos = undefined;
+
+        if(nodeCache.keys() === [])
+        {
+            var dataBase = new Database();
+
+            formacaoAcademica = await dataBase.getAllElements('formacao_academica');
+            conhecimentosTecnicos = await dataBase.getAllElements('conhecimentos_tecnicos');
+            contactos = await dataBase.getAllElements('	contactos');
+            experiencia = await dataBase.getAllElements('experiencia');
+            idiomas = await dataBase.getAllElements('idiomas');
+            linguagensFerramentas = await dataBase.getAllElements('linguagens_ferramentas');
+            projectos = await dataBase.getAllElements('projectos');
+            
+            console.log(contactos);
+            
+            // nodeCache.set('experiencia', experiencia);
+            // nodeCache.set('conhecimentosTecnicos', conhecimentosTecnicos);
+            // nodeCache.set('contactos', contactos);
+            // nodeCache.set('idiomas', idiomas);
+            // nodeCache.set('linguagensFerramentas', linguagensFerramentas);
+            // nodeCache.set('projectos', projectos);
+            // nodeCache.set('formacaoAcademica', formacaoAcademica);
+
+        }else
+        {
+            experiencia = nodeCache.get('experiencia');
+            conhecimentosTecnicos = nodeCache.get('conhecimentosTecnicos');
+            contactos = nodeCache.get('contactos');
+            idiomas = nodeCache.get('idiomas');
+            linguagensFerramentas = nodeCache.get('linguagensFerramentas');
+            projectos = nodeCache.get('projectos');
+            formacaoAcademica = nodeCache.get('formacaoAcademica');
+        }
+        let title = 'Admin Page';
+        console.log(nodeCache.keys());
+
+        /*
+        
+        let title = 'Admin Page';
+
+        
+        response.render('admin/index',{
+            formacaoAcademica,
+            conhecimentosTecnicos,
+            contactos,
+            experiencia,
+            idiomas,
+            linguagensFerramentas,
+            projectos,
+            title
+        });
+        */
     }else
     {
         response.redirect('/');
