d8a30f5f92d4a41d9930fdaeab09ef94a43a6cae
Roddas <roddas360@gmail.com>
2021-06-30 15:30:55 +0100

    Added feature for AJAX requests using feth API and some improvements related to the user interation

--- public/js/asset.js
+++ public/js/asset.js
@@ -33,3 +33,63 @@ let tabsWithContent = (function () {
     })
     tabs[0].click();
 })();
+
+let flag = true;
+let editButton = document.querySelector('.edit');
+let deleteButton = document.querySelector('.delet');
+let sendButton = document.createElement('a');
+sendButton.href = '#';
+sendButton.setAttribute('class','send');
+sendButton.textContent = 'Salvar';
+let textoElemento = document.querySelector('.texto');
+let textBox = document.createElement('input');
+textBox.type = 'text';
+textBox.required = true;
+textBox.name = 'descricao';
+textBox.className = 'text-edit input';
+let texto = textoElemento.textContent;
+textBox.setAttribute('name', 'texto');
+
+editButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (flag)
+    {
+        editButton.textContent = 'Cancelar';
+        textoElemento.replaceWith(textBox);
+        deleteButton.replaceWith(sendButton);
+        textBox.value = textoElemento.textContent;
+        flag = false;
+    }else
+    {
+        editButton.textContent = 'Editar';
+        textBox.replaceWith(textoElemento);
+        sendButton.replaceWith(deleteButton);
+        textoElemento.textContent = texto;
+        flag = true;
+    }
+});
+sendButton.addEventListener('click', e =>
+{
+    e.preventDefault();
+   (async ()=>{
+       var header = new Headers();
+       header.append('Content-Type','application/json');
+       let hidden = document.querySelector('.hidden').getAttribute('value');
+       let id = document.querySelector('.id').textContent;
+       console.log(id);
+       var metaData = {
+           method: 'PATCH',
+           headers: header,
+           body: JSON.stringify({ descricao: textBox.value, table : hidden,idElement : id})
+       }
+       try 
+       {
+           let result = await fetch('#', metaData);
+           console.log(result);
+       }
+       catch (e) 
+       {
+           console.log(e);
+       }
+   })();
+});
