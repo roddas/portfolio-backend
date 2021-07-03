25d7ece3777754e37894e9e2792172375c25474b
Roddas <roddas360@gmail.com>
2021-07-03 21:43:18 +0100

    Refactoring

--- public/js/asset.js
+++ public/js/asset.js
@@ -1,5 +1,7 @@
 var date = new Date();
 document.getElementById("ano").innerText = date.getFullYear();
+var header = new Headers();
+header.append('Content-Type', 'application/json');
 let tabsWithContent = (function () {
     let tabs = document.querySelectorAll('.tabs li');
     let tabsContent = document.querySelectorAll('.tab-content');
@@ -35,90 +37,29 @@ let tabsWithContent = (function () {
 })();
 
 let flag = true;
-let editButton = document.querySelector('.edit');
-let deleteButton = document.querySelector('.delet');
-let sendButton = document.createElement('a');
-sendButton.href = '#';
-sendButton.setAttribute('class','send');
-sendButton.textContent = 'Salvar';
-let textoElemento = document.querySelector('.texto');
-let textBox = document.createElement('input');
-textBox.type = 'text';
-textBox.required = true;
-textBox.name = 'descricao';
-textBox.className = 'text-edit input';
-let texto = textoElemento.textContent;
-textBox.setAttribute('name', 'texto');
 
-editButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    if (flag)
-    {
-        editButton.textContent = 'Cancelar';
-        textoElemento.replaceWith(textBox);
-        deleteButton.replaceWith(sendButton);
-        textBox.value = textoElemento.textContent;
-        flag = false;
-    }else
-    {
-        editButton.textContent = 'Editar';
-        textBox.replaceWith(textoElemento);
-        sendButton.replaceWith(deleteButton);
-        textoElemento.textContent = texto;
-        flag = true;
-    }
-});
-sendButton.addEventListener('click', e =>
+function submit(element)
 {
-    e.preventDefault();
-   (async ()=>{
-       var header = new Headers();
-       header.append('Content-Type','application/json');
-       let hidden = document.querySelector('.hidden').getAttribute('value');
-       let id = document.querySelector('.id').textContent;
-       var metaData = {
-           method: 'POST',
-           headers: header,
-           body: JSON.stringify({ descricao: textBox.value, table : hidden,idElement : id})
-       }
-       try
-       {
-           let result = await fetch('/admin/edit', metaData);
-           let response = await result.json();
-           alert(response.message);
+    var element = document.getElementById(element);
+    element.addEventListener('submit',e =>{
+        e.preventDefault();
+        let form = new FormData(element);
+        let data = {};
+        form.forEach((value, key) =>{ data[key] = value });
+       (async ()=>{
+           
+        await fetch('/admin/add', {
+               method: 'POST',
+               headers: header,
+               body: JSON.stringify(data)
+           });
+           let inputs = document.querySelectorAll('input');
+           for (i of inputs) 
+           {
+               i.value = '';
+           }
            location.reload();
-       }
-       catch(e)
-       {
-           console.error(e);
-       }
-
-   })();
-});
-
-deleteButton.addEventListener('click', e => {
-    e.preventDefault();
-    (async () => {
-        var header = new Headers();
-        header.append('Content-Type', 'application/json');
-        let hidden = document.querySelector('.hidden').getAttribute('value');
-        let id = document.querySelector('.id').textContent;
-        var metaData = {
-            method: 'POST',
-            headers: header,
-            body: JSON.stringify({table: hidden, idElement: id })
-        }
-        try
-        {
-            let result = await fetch('/admin/delete', metaData);
-            let response = await result.json();
-            alert(response.message);
-            location.reload();
-        }
-        catch (e)
-        {
-            console.error(e);
-        }
-
-    })();
-});
+       })();
+    });
+}
+submit('formacaoAcademicaForm');
