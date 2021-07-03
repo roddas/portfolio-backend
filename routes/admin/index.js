08d5ab6a732a3a28183219bb930d5a30f732bb12
Roddas <roddas360@gmail.com>
2021-07-03 21:44:29 +0100

    Updating the admin panel

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -12,22 +12,22 @@ var router = express.Router();
 router.get('/',async (request,response) =>{
     if (request.session.token === (process.env.TOKEN + process.env.TOKEN2) )
     {
-        let projectosObj = new Projectos();
-        let conhecimentosTecnicosObj = new ConhecimentosTecnicos();
-        let contactosObj = new Contactos();
-        let experienciaObj = new Experiencia();
-        let linguagensFerramentasObj = new LinguagensFerramentas();
-        let formacaoAcademicaObj = new FormacaoAcademica();
-        let idiomasObj = new Idiomas();
+        let projectos = new Projectos();
+        let conhecimentosTecnicos = new ConhecimentosTecnicos();
+        let contactos = new Contactos();
+        let experiencia = new Experiencia();
+        let linguagensFerramentas = new LinguagensFerramentas();
+        let formacaoAcademica = new FormacaoAcademica();
+        let idiomas = new Idiomas();
 
         let title = 'Admin Page';
-        let formacaoAcademica = await formacaoAcademicaObj.getFormacaoAcademica();
-        let conhecimentosTecnicos = await conhecimentosTecnicosObj.getConhecimentosTecnicos();
-        let contactos = await contactosObj.getContactos();
-        let experiencia = await experienciaObj.getExperiencia();
-        let idiomas = await idiomasObj.getIdiomas();
-        let linguagensFerramentas = await linguagensFerramentasObj.getLinguagensFerramentas();
-        let projectos = await projectosObj.getProjectos();
+        formacaoAcademica = await formacaoAcademica.getFormacaoAcademica();
+        conhecimentosTecnicos = await conhecimentosTecnicos.getConhecimentosTecnicos();
+        contactos = await contactos.getContactos();
+        experiencia = await experiencia.getExperiencia();
+        idiomas = await idiomas.getIdiomas();
+        linguagensFerramentas = await linguagensFerramentas.getLinguagensFerramentas();
+        projectos = await projectos.getProjectos();
                 
         response.render('admin/index', {
             formacaoAcademica,
