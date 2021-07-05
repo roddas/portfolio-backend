66652795d51e3a080781c5381d5d2ae7433bd4f2
Roddas <roddas360@gmail.com>
2021-07-05 23:31:11 +0100

    Refactoring

--- routes/admin/edit.js
+++ routes/admin/edit.js
@@ -1,73 +1,104 @@
 const express = require('express');
 
 var router = express.Router();
-var tableClass = new Map();
-
-tableClass.set('conhecimentos_tecnicos', async (idElement, descricao)=>
-{
-    const { ConhecimentosTecnicos } = require('../../bin/databasesClasses/autoloader');
-    let conhecimentosTecnicos = new ConhecimentosTecnicos();
-    await conhecimentosTecnicos.updateFormacaoAcademica(idElement, descricao);
-});
-tableClass.set('contactos', async (idElement, descricao)=>{
-    const { Contactos } = require('../../bin/databasesClasses/autoloader');
-    let contactos = new Contactos();
-    await contactos.updateContacto(idElement,descricao);
-});
-
-tableClass.set('experiencia', async (idElement,descricao) =>{
-    const { Experiencia } = require('../../bin/databasesClasses/autoloader');
-    let experiencia = new Experiencia();
-    await experiencia.updateExperiencia(idElement,descricao);
-});
-tableClass.set('formacao_academica', async (idElement,descricao) =>{
-    const { FormacaoAcademica } = require('../../bin/databasesClasses/autoloader');
-    let formacaoAcademica = new FormacaoAcademica();
-    await formacaoAcademica.updateFormacaoAcademica(idElement,descricao);
-});
-tableClass.set('idiomas', async (idElement,descricao) =>{
-    const { Idiomas } = require('../../bin/databasesClasses/autoloader');
-    const idiomas = new Idiomas();
-    idiomas.updateIdioma(idElement,descricao);
-});
-tableClass.set('linguagens_ferramentas', async (idElement,descricao) =>{
-    const { LinguagensFerramentas } = require('../../bin/databasesClasses/autoloader');
-    let linguagensFerramentas = new LinguagensFerramentas();
-    await linguagensFerramentas.updateLinguagemFerramenta(idElement,descricao);
-});
-tableClass.set('projectos', async (nome, descricao, estado, link, idElement)=>{
-    const { Projectos } = require('../../bin/databasesClasses/autoloader');
-    let projectos = new Projectos();
-    await projectos.updateProjecto(nome,descricao,estado,link,idElement);
-});
 
 router.get('/:table/:id',async (request,response) =>{
     if (request.session.token === (process.env.TOKEN + process.env.TOKEN2) )
     {
-        const {table,id} = request.params;
-        if(table.length === 0 || id.length === 0)
-        {
-            response.redirect('/');
-        }
-        response.render('edit',{table});
+        var tableClass = new Map();
+
+        tableClass.set('conhecimentos_tecnicos', async (id) => {
+            const { ConhecimentosTecnicos } = require('../../bin/databasesClasses/autoloader');
+            let conhecimentosTecnicos = new ConhecimentosTecnicos();
+            await conhecimentosTecnicos.getConhecimentosTecnicosById(id);
+        });
+        tableClass.set('contactos', async (id) => {
+            const { Contactos } = require('../../bin/databasesClasses/autoloader');
+            let contactos = new Contactos();
+            await contactos.getContactosById(id);
+        });
+
+        tableClass.set('experiencia', async (id) => {
+            const { Experiencia } = require('../../bin/databasesClasses/autoloader');
+            let experiencia = new Experiencia();
+            await experiencia.getExperienciaById(id);
+        });
+        tableClass.set('formacao_academica', async (id) => {
+            const { FormacaoAcademica } = require('../../bin/databasesClasses/autoloader');
+            let formacaoAcademica = new FormacaoAcademica();
+            await formacaoAcademica.getFormacaoAcademicaById(id);
+        });
+        tableClass.set('idiomas', async (id) => {
+            const { Idiomas } = require('../../bin/databasesClasses/autoloader');
+            const idiomas = new Idiomas();
+            idiomas.getIdiomaById(id);
+        });
+        tableClass.set('linguagens_ferramentas', async (id,) => {
+            const { LinguagensFerramentas } = require('../../bin/databasesClasses/autoloader');
+            let linguagensFerramentas = new LinguagensFerramentas();
+            await linguagensFerramentas.getLinguagemFerramentaById(id);
+        });
+        tableClass.set('projectos', async (id) => {
+            const { Projectos } = require('../../bin/databasesClasses/autoloader');
+            let projectos = new Projectos();
+            await projectos.getProjectoById(id);
+        });
+        const { id, table } = request.params;
+        let objectClass = tableClass.get(table);
+        let data = await objectClass(id);
+        request.session.table = table;
+        response.render('admin/edit',{data});
+
     }else
     {
         response.redirect('/');
     }
 });
-router.get('/:table/:id', async (request, response) => {
+
+router.post('/', async (request, response) => {
     if (request.session.token === (process.env.TOKEN + process.env.TOKEN2)) {
-        const { nome, descricao, estado, link, idElement, table } = request.body;
-        let objectClass = tableClass.get(table);
-        if (objectClass === undefined) {
-            response.redirect('/');
-        }
-        if (table !== 'projectos') {
-            await objectClass(idElement, descricao);
-        } else {
-            await objectClass(nome, descricao, estado, link, idElement);
-        }
-        response.json(result);
+        var tableClass = new Map();
+
+        tableClass.set('conhecimentos_tecnicos', async (id,descricao) => {
+            const { ConhecimentosTecnicos } = require('../../bin/databasesClasses/autoloader');
+            let conhecimentosTecnicos = new ConhecimentosTecnicos();
+            await conhecimentosTecnicos.updateConhecimentosTecnicos(id, descricao);
+        });
+        tableClass.set('contactos', async (id,tipo,descricao) => {
+            const { Contactos } = require('../../bin/databasesClasses/autoloader');
+            let contactos = new Contactos();
+            await contactos.updateContacto(id,tipo,descricao);
+        });
+
+        tableClass.set('experiencia', async (id,descricao) => {
+            const { Experiencia } = require('../../bin/databasesClasses/autoloader');
+            let experiencia = new Experiencia();
+            await experiencia.updateExperiencia(id,descricao);
+        });
+        tableClass.set('formacao_academica', async (id, descricao) => {
+            const { FormacaoAcademica } = require('../../bin/databasesClasses/autoloader');
+            let formacaoAcademica = new FormacaoAcademica();
+            await formacaoAcademica.updateFormacaoAcademica(id,descricao);
+        });
+        tableClass.set('idiomas', async (id,descricao) => {
+            const { Idiomas } = require('../../bin/databasesClasses/autoloader');
+            const idiomas = new Idiomas();
+            idiomas.updateIdioma(id,descricao);
+        });
+        tableClass.set('linguagens_ferramentas', async (id,descricao) => {
+            const { LinguagensFerramentas } = require('../../bin/databasesClasses/autoloader');
+            let linguagensFerramentas = new LinguagensFerramentas();
+            await linguagensFerramentas.updateLinguagemFerramenta(id,descricao);
+        });
+        tableClass.set('projectos', async (nome, descricao, estado, link, id) => {
+            const { Projectos } = require('../../bin/databasesClasses/autoloader');
+            let projectos = new Projectos();
+            await projectos.updateProjecto(nome,descricao,estado,link,id);
+        });
+        const { id,nome, descricao, estado, link } = request.body;
+        const table = request.session.table;
+        console.log(table);
+        
     } else {
         response.redirect('/');
     }
--- routes/admin/index.js
+++ routes/admin/index.js
@@ -21,14 +21,23 @@ router.get('/',async (request,response) =>{
         let idiomas = new Idiomas();
 
         let title = 'Admin Page';
-        formacaoAcademica = await formacaoAcademica.getFormacaoAcademica();
-        conhecimentosTecnicos = await conhecimentosTecnicos.getConhecimentosTecnicos();
-        contactos = await contactos.getContactos();
-        experiencia = await experiencia.getExperiencia();
-        idiomas = await idiomas.getIdiomas();
-        linguagensFerramentas = await linguagensFerramentas.getLinguagensFerramentas();
-        projectos = await projectos.getProjectos();
-                
+        request.session.data = true;
+        request.session.formacaoAcademica = await formacaoAcademica.getFormacaoAcademica();
+        request.session.conhecimentosTecnicos = await conhecimentosTecnicos.getConhecimentosTecnicos();
+        request.session.contactos = await contactos.getContactos();
+        request.session.experiencia = await experiencia.getExperiencia();
+        request.session.idiomas = await idiomas.getIdiomas();
+        request.session.linguagensFerramentas = await linguagensFerramentas.getLinguagensFerramentas();
+        request.session.projectos = await projectos.getProjectos();
+        
+        formacaoAcademica = request.session.formacaoAcademica;
+        contactos = request.session.contactos;
+        experiencia = request.session.experiencia;
+        projectos = request.session.projectos;
+        linguagensFerramentas = request.session.linguagensFerramentas;
+        idiomas = request.session.idiomas;
+        conhecimentosTecnicos = request.session.conhecimentosTecnicos;
+
         response.render('admin/index', {
             formacaoAcademica,
             conhecimentosTecnicos,
