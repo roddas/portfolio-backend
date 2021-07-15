abbc5047e6edea22dbd367ea72ed338b22732630
Roddas <roddas360@gmail.com>
2021-07-15 23:07:15 +0100

    Improving the admin route

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,59 +1,10 @@
 const express = require('express');
-const { Projectos } = require('../../bin/databasesClasses/autoloader');
-const { ConhecimentosTecnicos } = require('../../bin/databasesClasses/autoloader');
-const { Contactos } = require('../../bin/databasesClasses/autoloader');
-const { Experiencia } = require('../../bin/databasesClasses/autoloader');
-const { FormacaoAcademica } = require('../../bin/databasesClasses/autoloader');
-const { Idiomas } = require('../../bin/databasesClasses/autoloader');
-const { LinguagensFerramentas } = require('../../bin/databasesClasses/autoloader');
-
 var router = express.Router();
 
-router.get('/',async (request,response) =>{
-    if (request.session.token === (process.env.TOKEN + process.env.TOKEN2) )
-    {
-        let projectos = new Projectos();
-        let conhecimentosTecnicos = new ConhecimentosTecnicos();
-        let contactos = new Contactos();
-        let experiencia = new Experiencia();
-        let linguagensFerramentas = new LinguagensFerramentas();
-        let formacaoAcademica = new FormacaoAcademica();
-        let idiomas = new Idiomas();
+const { renderIndex } = require('../../controllers/indexController');
+const loginMiddleware = require('../../middlewares/checkLogin');
 
-        let title = 'Admin Page';
-        request.session.data = true;
-        request.session.formacaoAcademica = await formacaoAcademica.getFormacaoAcademica();
-        request.session.conhecimentosTecnicos = await conhecimentosTecnicos.getConhecimentosTecnicos();
-        request.session.contactos = await contactos.getContactos();
-        request.session.experiencia = await experiencia.getExperiencia();
-        request.session.idiomas = await idiomas.getIdiomas();
-        request.session.linguagensFerramentas = await linguagensFerramentas.getLinguagensFerramentas();
-        request.session.projectos = await projectos.getProjectos();
-        
-        formacaoAcademica = request.session.formacaoAcademica;
-        contactos = request.session.contactos;
-        experiencia = request.session.experiencia;
-        projectos = request.session.projectos;
-        linguagensFerramentas = request.session.linguagensFerramentas;
-        idiomas = request.session.idiomas;
-        conhecimentosTecnicos = request.session.conhecimentosTecnicos;
-
-        response.render('admin/index', {
-            formacaoAcademica,
-            conhecimentosTecnicos,
-            contactos,
-            experiencia,
-            idiomas,
-            linguagensFerramentas,
-            projectos,
-            title
-        });
-        
-    }else
-    {
-        response.redirect('/');
-    }
-});
+router.get('/', loginMiddleware,renderIndex);
 
 router.post('/',async (request,response) =>{
     if (request.session.token === (process.env.TOKEN + process.env.TOKEN2))
