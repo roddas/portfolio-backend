e29b58bb96ead8518aa574bf0a3de75645557ddb
Rodolfo Cabral <roddas360@gmail.com>
2022-04-15 21:35:22 -0300

    working on projects CRUD

--- controllers/projectsController.js
+++ controllers/projectsController.js
@@ -1,13 +1,21 @@
 import { ProjectsModel } from '../models/ProjectsModel';
+import {ServerError} from '../helper/errorHandling';
 
 let projectsModel = new ProjectsModel();
+let serverErrorHandle = new ServerError();
 
 export class ProjectsController
 {
     async readProjects(request, response)
     {
-        let projects = await projectsModel.getProjects();
-        response.json(projects);
+       try
+       {
+           let projects = await projectsModel.getProjects();
+           response.json(projects);
+       }catch(error)
+       {
+           serverErrorHandle.internalServerError(error,response);
+       }
     }
     async postProject(request, response) { }
     async updateProject(request, response) { }
